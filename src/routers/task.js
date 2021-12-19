7d2052f16677b46644540c1ed510588dabe73fc2
Omid <o.shabani@hotmail.com>
2021-12-19 22:42:02 +0330

    implement sort for task

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,23 +19,32 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=2&skip=2
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     try {
         // const tasks = await Task.find({ owner: req.user._id })
         // res.send(tasks)
         const match = {}
+        const sort = {}
         if (req.query.completed) {
             match.completed = req.query.completed === 'true'
         }
 
+        if(req.query.sortBy){
+            const parts = req.query.sortBy.split(':') 
+            sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+        }
+        
         //Second Way
         await req.user.populate({
             path: 'tasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
-            }
+                skip: parseInt(req.query.skip),
+                sort:sort
+            },
+           
         })
         res.send(req.user.tasks)
 
