101227e61c0e13fde746fe2129cddc4e4f4a05fc
Omid <o.shabani@hotmail.com>
2021-12-29 23:26:31 +0330

    add validation to avatar upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -118,7 +118,17 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits:{
+        fileSize: 10000
+    },
+    fileFilter(req,file,cb){
+
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            cb(new Error('file must be image'))
+        }
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
