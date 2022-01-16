b11e3b9a0065ef31834ddcfc218d6755314dcc00
Omid <o.shabani@hotmail.com>
2022-01-16 12:17:33 +0330

    add email cancelation to delete user

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
-const { sendWelcomeEmail } = require('../emails/account')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const multer = require('multer')
 const sharp = require('sharp')
 const router = express.Router()
@@ -112,6 +112,7 @@ router.delete('/users/me', auth, async (req, res) => {
         // await req.user.delete()
 
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.password)
         res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
