8f4a55c62e0eff22a792b6f619592a0260a49ae6
Omid <o.shabani@hotmail.com>
2022-01-17 18:49:27 +0330

    create test for get user tasks

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
-
-const sampleUserId = new mongoose.Types.ObjectId
-sampleUser = {
+const sampleUserId = new mongoose.Types.ObjectId()
+const sampleUser = {
     _id: sampleUserId,
     name: 'Sara',
     email: 'sara@example.com',
@@ -14,9 +14,41 @@ sampleUser = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'Reza',
+    email: 'reza@example.com',
+    password: 'Red123@@',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }]
+}
+
+const taskOne = {   
+    description: 'First Task',
+    completed: false,
+    owner: sampleUser._id
+}
+const taskTwo = {   
+    description: 'Second Task',
+    completed: true,
+    owner: sampleUser._id
+}
+const taskThree = {   
+    description: 'Third Task',
+    completed: true,
+    owner: userTwo._id
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(sampleUser).save()
+    await new User(userTwo).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
--- tests/task.test.js
+++ tests/task.test.js
@@ -8,14 +8,24 @@ beforeEach(setupDatabase)
 test('Should create task for user', async () => {
     const response = await request(app)
         .post('/tasks')
-        .set('Authorization',`Bearer ${sampleUser.tokens[0].token}`)
+        .set('Authorization', `Bearer ${sampleUser.tokens[0].token}`)
         .send({
             description: 'New test Task'
         })
         .expect(201)
-        
-        const task = await Task.findById(response.body._id)
-        expect(task).not.toBeNull()
-        expect(task.completed).toEqual(false)
+
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
+})
+
+test('Should fetch user tasks', async () => {
+    const response = await request(app)
+    .get('/tasks')
+    .set('Authorization',`Bearer ${sampleUser.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+    expect(response.body.length).toEqual(2)
 })
 
